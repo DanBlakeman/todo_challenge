ae66b43d3dea6944f083bd13db7dfbfd7baf80b7
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-05-01 18:01:05 +0100

    written failing feature test for marking complete

---
+++ test/e2e/markCompletedFeature.js
@@ -0,0 +1,23 @@
+describe('To do list app', function() {
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+    var input = element(by.model('Main.task'));
+    input.sendKeys('my first task');
+    element(by.buttonText('Add Task!')).click();
+    var input = element(by.model('Main.task'));
+    input.sendKeys('my second task');
+    element(by.buttonText('Add Task!')).click();
+  });
+
+  it("I can mark completed items", function() {
+    first_item = element.all(by.repeater('task in Main.taskList')).get(0);
+    expect(first_item)
+    first_item.click();
+    expect(first_item.getAttribute('class')).toContain('task_item--complete')
+  });
+
+
+
+
+});
--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -11,7 +11,18 @@ describe('To do list app', function() {
   it("I can post a task to the page", function() {
     var input = element(by.model('Main.task'));
     input.sendKeys('my first task');
-    expect(element(by.id('taskList')).getText()).toEqual('my first task');
+    element(by.buttonText('Add Task!')).click();
+    expect(element(by.repeater('task in Main.taskList')).getText()).toEqual('my first task');
+  });
+
+  it("I can post multiple tasks to the page", function() {
+    var input = element(by.model('Main.task'));
+    input.sendKeys('my first task');
+    element(by.buttonText('Add Task!')).click();
+    input.sendKeys('my second task');
+    element(by.buttonText('Add Task!')).click();
+    expect(element.all(by.repeater('task in Main.taskList')).get(0).getText()).toEqual('my first task');
+    expect(element.all(by.repeater('task in Main.taskList')).get(1).getText()).toEqual('my second task');
   });
 
 
