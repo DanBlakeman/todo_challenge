32301cf66be2ff52bb5110cfcd87fae74148c5a6
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-05-02 19:38:01 +0100

    added port var for heroku to server

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+var port = process.env.PORT || 3000;
+
 server.listen(3000, function(){
-  console.log("Understood Sir, Standing Watch Over Port 3000");
+  console.log('Understood Sir, Standing Watch Over Port ' + 3000);
 });
 
 app.use(express.static(__dirname + '/public'));
